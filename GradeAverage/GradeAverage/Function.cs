f42f8755ddc5949bc9f91dc9882ff316781fa78e
nlthorne <nlthorne@wi.rr.com>
2016-10-17 07:48:04 -0500

    moving files into proper folder

---
+++ GradeAverage/GradeAverage/Function.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeAverage
+{
+    class Function
+    {
+
+        public Function()
+        {
+
+        }
+        
+        public void GetAverage()
+        {
+            string grades = "90,100,82,89,55";
+            var numbers = grades.Split(',').Select(Int32.Parse).ToList();
+            numbers.Sort();
+            double result = (from x in numbers where x > numbers[2] select x).Average();
+            Console.WriteLine(result);
+        }
+    }
+}
