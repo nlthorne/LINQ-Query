c47d6f80aade3b7832a86cb87030c496e67b34ae
nlthorne <nlthorne@wi.rr.com>
2016-12-02 16:59:58 -0600

    fixed the program file

--- GradeAverage/GradeAverage/Program.cs
+++ GradeAverage/GradeAverage/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Duplicates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace GradeAverage
         {
             GradeAverage gradeAverage = new GradeAverage();
             gradeAverage.DisplayResults();
+            Console.WriteLine("\n ================================");
+            RemoveDuplicate removeDuplicate = new RemoveDuplicate();
+            removeDuplicate.DisplayResults();
             Console.ReadLine();
         }
     }
---
+++ GradeAverage/GradeAverage/RemoveDuplicate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicates
+{
+    public class RemoveDuplicate
+    {
+        private List<string> listOfStrings;
+
+        public RemoveDuplicate()
+        {
+            listOfStrings = new List<string>() { "Test", "Hello", "Hello", "World", "!!!", "!!!", "Test" };
+        }
+
+        public void DisplayResults()
+        {
+
+            Console.Write("Original list: ");
+            foreach (string value in listOfStrings)
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.Write("\nNew list without duplicates: ");
+            var result = listOfStrings.Distinct();
+            foreach (string value in result)
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
